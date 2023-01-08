05197694d6a93c70fc7e6e8ad166510b2c715a08
hanwoolk <119464040+hanwoolk@users.noreply.github.com>
2023-01-08 23:36:21 +0900

    Update DDL.sql

--- source/01_Java/lastEx/src/com/lec/ex3supermarket/DDL.sql
+++ source/01_Java/lastEx/src/com/lec/ex3supermarket/DDL.sql
@@ -1,43 +1,52 @@
---1. È¸¿ø°¡ÀÔ : ÀüÈ­¹øÈ£¿Í ÀÌ¸§À» ÀÔ·Â¹Þ¾Æ È¸¿ø°¡ÀÔ(È¸¿ø°¡ÀÔ½Ã Æ÷ÀÎÆ®´Â 1000Á¡ Á¦°ø
+--1. íšŒì›ê°€ìž… : ì „í™”ë²ˆí˜¸ì™€ ì´ë¦„ì„ ìž…ë ¥ë°›ì•„ íšŒì›ê°€ìž…(íšŒì›ê°€ìž…ì‹œ í¬ì¸íŠ¸ëŠ” 1000ì  ì œê³µ
 INSERT INTO CUSTOMER (CID,CTEL,CNAME) 
-    VALUES (CSEQ.NEXTVAL,'010-6666-7777','È«±æµ¿');
+    VALUES (CSEQ.NEXTVAL,'010-6666-7777','í™ê¸¸ë™');
 
--- 2. Æù4ÀÚ¸®(FULL) °Ë»ö
-    -- (1)Æù µÚ4ÀÚ¸®³ª FULL¹øÈ£¸¦ ÀÔ·Â¹Þ¾Æ °í°´Á¤º¸(ID, ÀüÈ­, ÀÌ¸§, Æ÷ÀÎÆ®, ±¸¸Å´©Àû¾×, °í°´·¹
-    -- º§, ·¹º§¾÷À» À§ÇÑ Ãß°¡ ±¸¸ÅÇÒ ±Ý¾×) ¸¦ ±¸¸Å´©Àû¾× Å« ¼øÀ¸·Î Ãâ·Â
-SELECT C.* ,LEVELNAME,(NVL(HIC,99999999999)-CAMOUNT+1) NEXTLEVEL FROM CUSTOMER C, CLEVEL WHERE CAMOUNT BETWEEN LOC AND HIC AND SUBSTR(CTEL,10,4)=1111 ;
+-- 2. í°4ìžë¦¬(FULL) ê²€ìƒ‰
+    -- (1)í° ë’¤4ìžë¦¬ë‚˜ FULLë²ˆí˜¸ë¥¼ ìž…ë ¥ë°›ì•„ ê³ ê°ì •ë³´(ID, ì „í™”, ì´ë¦„, í¬ì¸íŠ¸, êµ¬ë§¤ëˆ„ì ì•¡, ê³ ê°ë ˆ
+    -- ë²¨, ë ˆë²¨ì—…ì„ ìœ„í•œ ì¶”ê°€ êµ¬ë§¤í•  ê¸ˆì•¡) ë¥¼ êµ¬ë§¤ëˆ„ì ì•¡ í° ìˆœìœ¼ë¡œ ì¶œë ¥
+SELECT C.* ,LEVELNAME,(NVL(HIC,CAMOUNT-1)-CAMOUNT+1) NEXTLEVEL 
+    FROM CUSTOMER C, CLEVEL
+        WHERE CAMOUNT BETWEEN LOC AND NVL(HIC,CAMOUNT+1)AND SUBSTR(CTEL,-4,4)=?
+	ORDER BY CAMOUNT DESC;
     
     
-    -- (2)Ãâ·Â °á°ú´Â 0Çà ÀÌ»ó
+    -- (2)ì¶œë ¥ ê²°ê³¼ëŠ” 0í–‰ ì´ìƒ
 
     
     
---3. ¹°Ç°±¸ÀÔ
-    -- °í°´id(¹øÈ£)¿Í ¹°Ç°±¸¸Å°¡°ÝÀ» ÀÔ·Â¹Þ¾Æ ±¸¸Å Ã³¸®.
-    -- (1) ±¸¸Å´©Àû±Ý¾×¿¡ ±Ý¹ø ±¸¸Å±Ý¾×ÀÌ ´©Àû
-    -- (2) ±Ý¹ø ±¸¸Å´©Àû±Ý¾×ÀÇ 5%Æ÷ÀÎÆ®°¡ ´©Àû
-    -- (3) º¯°æµÉ ±¸¸Å´©Àû±Ý¾×¿¡ µû¶ó °í°´ÀÇ ·¹º§ÀÌ »óÇâÁ¶Á¤µÉ °æ¿ì °í°´ ·¹º§ÀÌ »óÇâ
+--3. ë¬¼í’ˆêµ¬ìž…
+    -- ê³ ê°id(ë²ˆí˜¸)ì™€ ë¬¼í’ˆêµ¬ë§¤ê°€ê²©ì„ ìž…ë ¥ë°›ì•„ êµ¬ë§¤ ì²˜ë¦¬.
+    -- (1) êµ¬ë§¤ëˆ„ì ê¸ˆì•¡ì— ê¸ˆë²ˆ êµ¬ë§¤ê¸ˆì•¡ì´ ëˆ„ì 
+    -- (2) ê¸ˆë²ˆ êµ¬ë§¤ëˆ„ì ê¸ˆì•¡ì˜ 5%í¬ì¸íŠ¸ê°€ ëˆ„ì 
+    -- (3) ë³€ê²½ë  êµ¬ë§¤ëˆ„ì ê¸ˆì•¡ì— ë”°ë¼ ê³ ê°ì˜ ë ˆë²¨ì´ ìƒí–¥ì¡°ì •ë  ê²½ìš° ê³ ê° ë ˆë²¨ì´ ìƒí–¥
+UPDATE CUSTOMER SET CAMOUNT = CAMOUNT + ? , CPOINT = CPOINT+(?*0.05) WHERE CID=?
     
     
     
     
-    
---4. °í°´ µî±Þº° Ãâ·Â
-    -- (1)¾î¶² ·¹º§À» ¿øÇÏ´ÂÁö µ¥ÀÌÅÍº£ÀÌ½ºÀÇ ·¹º§ÀÌ¸§À» Ãâ·ÂÇÏ¸ç Æ¯Á¤ ·¹º§¸íÀ» ÀÔ·Â¹Þ´Â´Ù.
-    -- (2)ÇØ´ç ·¹º§ °í°´Á¤º¸(ID, ÀüÈ­, ÀÌ¸§, Æ÷ÀÎÆ®, ±¸¸Å´©Àû¾×, °í°´·¹º§, ·¹º§¾÷À» À§ÇÑ Ãß°¡ ±¸
-    -- ¸ÅÇÒ ±Ý¾×)¸¦ ±¸¸Å´©Àû¾× Å« ¼øÀ¸·Î Ãâ·Â
-    
-    
-    
---5. ÀüÃ¼ Ãâ·Â
-    -- ÀüÃ¼ °í°´Á¤º¸(ID, ÀüÈ­, ÀÌ¸§, Æ÷ÀÎÆ®, ±¸¸Å´©Àû¾×, °í°´·¹º§, ·¹º§¾÷À» À§ÇÑ Ãß°¡ ±¸¸ÅÇÒ
-    -- ±Ý¾×)¸¦ ±¸¸Å´©Àû¾× Å« ¼øÀ¸·Î Ãâ·Â
+--4. ê³ ê° ë“±ê¸‰ë³„ ì¶œë ¥
+    -- (1)ì–´ë–¤ ë ˆë²¨ì„ ì›í•˜ëŠ”ì§€ ë°ì´í„°ë² ì´ìŠ¤ì˜ ë ˆë²¨ì´ë¦„ì„ ì¶œë ¥í•˜ë©° íŠ¹ì • ë ˆë²¨ëª…ì„ ìž…ë ¥ë°›ëŠ”ë‹¤.
+    -- (2)í•´ë‹¹ ë ˆë²¨ ê³ ê°ì •ë³´(ID, ì „í™”, ì´ë¦„, í¬ì¸íŠ¸, êµ¬ë§¤ëˆ„ì ì•¡, ê³ ê°ë ˆë²¨, ë ˆë²¨ì—…ì„ ìœ„í•œ ì¶”ê°€ êµ¬
+    -- ë§¤í•  ê¸ˆì•¡)ë¥¼ êµ¬ë§¤ëˆ„ì ì•¡ í° ìˆœìœ¼ë¡œ ì¶œë ¥
+SELECT C.* , LEVELNAME, (NVL(HIC,CAMOUNT-1)-CAMOUNT+1) NEXTLEVEL
+				FROM CUSTOMER C, CLEVEL
+                    WHERE CAMOUNT BETWEEN LOC AND NVL(HIC,CAMOUNT+1) AND LEVELNAME = ?
+				ORDER BY CAMOUNT DESC; 
     
     
+--5. ì „ì²´ ì¶œë ¥
+    -- ì „ì²´ ê³ ê°ì •ë³´(ID, ì „í™”, ì´ë¦„, í¬ì¸íŠ¸, êµ¬ë§¤ëˆ„ì ì•¡, ê³ ê°ë ˆë²¨, ë ˆë²¨ì—…ì„ ìœ„í•œ ì¶”ê°€ êµ¬ë§¤í• 
+    -- ê¸ˆì•¡)ë¥¼ êµ¬ë§¤ëˆ„ì ì•¡ í° ìˆœìœ¼ë¡œ ì¶œë ¥
+SELECT C.* , LEVELNAME, (NVL(HIC,CAMOUNT-1)-CAMOUNT+1) NEXTLEVEL
+	FROM CUSTOMER C, CLEVEL
+	WHERE CAMOUNT BETWEEN LOC AND NVL(HIC,CAMOUNT+1)
+	ORDER BY CAMOUNT DESC;
     
     
---6. È¸¿øÅ»Åð ? ÀüÈ­¹øÈ£¸¦ ÀÔ·Â¹Þ¾Æ Å»ÅðÃ³¸®
     
+--6. íšŒì›íƒˆí‡´ ? ì „í™”ë²ˆí˜¸ë¥¼ ìž…ë ¥ë°›ì•„ íƒˆí‡´ì²˜ë¦¬
+DELETE FROM CUSTOMER WHERE CTEL=?;
     
     
     
