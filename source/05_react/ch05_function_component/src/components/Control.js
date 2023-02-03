39be864a2833c2117f82904377ce10ace69cf305
hanwoolk <hanwool.kim0312@gmail.com>
2023-02-03 15:55:39 +0900

    230203

---
+++ source/05_react/ch05_function_component/src/components/Control.js
@@ -0,0 +1,30 @@
+import "./Control.css";
+const Control = props => {
+  return (
+    <ul className="Control">
+      <li>
+        <button onClick={()=>{
+          props.onChangeMode('create');
+        }}>
+          CREATE
+        </button>
+      </li>
+      <li>
+        <button onClick={()=>{
+          props.onChangeMode('update');
+        }}>
+          UPDATE
+          </button>
+      </li>
+      <li>
+        <button onClick={()=>{
+          props.onChangeMode('delete');
+        }}>
+          DELETE
+          </button>
+      </li>
+    </ul>
+  );
+};
+
+export default Control;
