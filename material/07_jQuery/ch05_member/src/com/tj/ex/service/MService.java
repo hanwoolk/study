8cf03f06249b4da379474cde41a3cc05a86d0bf6
hanwoolk <hanwool.kim0312@gmail.com>
2023-06-22 20:24:50 +0900

    230622

--- material/07_jQuery/ch05_member/src/com/tj/ex/service/MService.java
+++
@@ -1,7 +0,0 @@
-package com.tj.ex.service;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-public interface MService {
-	public void execute(HttpServletRequest request,
-			HttpServletResponse response);
-}
